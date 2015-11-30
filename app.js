da1e1081083e39300dbd222b9e1d2d52a6e1ec89
Marko Golec <marko.golec@gmail.com>
2015-11-30 18:31:45 +0100

    04-010

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
 var app = angular.module('todoApp', []);
 
-app.controller('IndexPageController', function ($scope) {
+app.controller('IndexPageController', function ($scope, API) {
     //app logic
 
-    $scope.list = [];
+    $scope.list = API.get();
     $scope.submit = function () {
-        $scope.list.push({
+        $scope.list = API.add ({
             name: $scope.newItem,
             done: false
         });
@@ -17,3 +17,27 @@ app.controller('IndexPageController', function ($scope) {
     };
 
 });
+
+app.factory('API', function () {
+    var list = [];
+
+    return {
+        get: function () {
+            return list;
+        },
+        add: function (item) {
+            list.push(item);
+            return list;
+        }
+    }
+
+});
+
+app.directive('todoItem', function () {
+
+    return {
+        template: '<h3 class="{{todo.done ? "line-through" : ""}}"><input type="checkbox" ng-model="todo.done"> {{todo.name}}</h3>'
+    }
+
+});
+
